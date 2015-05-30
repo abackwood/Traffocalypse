d26c3495adab2833a5a3cdc2660ef82db34cd15a
abackwood <a.backwood@gmail.com>
2015-05-30 14:03:30 +0200

    Added simple graphics for sources and sinks

---
+++ Traffic Flow Simulator/Assets/CarSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarSpawner : MonoBehaviour {
+	public Car template;
+	public int carsToSpawn;
+
+	List<Car> cars;
+
+	// Use this for initialization
+	void Start () {
+		cars = new List<Car>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while(cars.Count < carsToSpawn) {
+			SourceSink[] sources = SourceSink.Sources;
+			SourceSink[] sinks = SourceSink.Sinks;
+
+			Car car = GameObject.Instantiate(template);
+			car.source = sources[UnityEngine.Random.Range(0,sources.Length)];
+			car.destination = sinks[UnityEngine.Random.Range (0,sinks.Length)];
+
+			Lane[] possibleLanes = car.source.road.OutLanes(car.source);
+			car.currentLane = possibleLanes[UnityEngine.Random.Range(0,possibleLanes.Length)];
+
+			cars.Add (car);
+		}
+	}
+}
