c5ab3e445a3063311724b4ed612e41bf05565e48
abackwood <a.backwood@gmail.com>
2015-06-02 11:42:57 +0200

    Test

--- Traffic Flow Simulator/Assets/Road.cs
+++ Traffic Flow Simulator/Assets/Road.cs
@@ -83,6 +83,10 @@ public class Road : MonoBehaviour {
 				LanesBack[j] = lane;
 			}
 		}
+
+	}
+	void InitLanes() {
+		
 	}
 
 	void CalculateSpeed()
@@ -104,86 +108,9 @@ public class Road : MonoBehaviour {
 		renderer.SetPosition(0,from.transform.position);
 		renderer.SetPosition(1,to.transform.position);
 	}
-	void InitLanes() {
-
-	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
-}
-
-public struct Lane {
-	Road road;
-	public Road Road {
-		get { return road; }
-	}
-
-	int id;
-	public int ID {
-		get { return id; }
-	}
-
-	Connection from;
-	public Connection From {
-		get { return from; }
-	}
-
-	Connection to;
-	public Connection To {
-		get { return to; }
-	}
-
-	public float Length
-	{
-		get { return road.length; }
-	}
-	
-	public float Speed
-	{
-		get { return road.speed; }
-	}
-	
-	public float SpeedX
-	{
-		get { return road.SpeedX; }
-	}
-	
-	public float SpeedY
-	{
-		get { return road.SpeedY; }
-	}
-
-	List<Car> carsOnLane;
-	public Car[] CarsOnLane {
-		get { return carsOnLane.ToArray(); }
-	}
-
-	public Lane(Road road, int id, Connection from, Connection to) {
-		this.road = road;
-		this.id = id;
-		this.from = from;
-		this.to = to;
-
-		carsOnLane = new List<Car>();
-	}
-
-	public void Subscribe(Car car)
-	{
-		int count = carsOnLane.Count - 1;
-		if (count > -1)
-			car.nextCar = carsOnLane[count];
-		carsOnLane.Add(car);
-	}
-	
-	public void Unsubcribe(Car car)
-	{
-		//Jag är inte galen, jag är ett flygplan!
-	}
-
-	public override string ToString ()
-	{
-		return Road.name + "-" + id + "(" + from.name + "-" + to.name + ")";
 	}
 }
