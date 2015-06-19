77608c0300ddc8a5e2b4a92ecf037ed5f6df1515
Timo Overbeek <timotheusoverbeek@gmail.com>
2015-06-19 16:24:28 +0200

    Implemented Collisions and working on how to resolve them

--- Traffic Flow Simulator/Assets/Car.cs
+++ Traffic Flow Simulator/Assets/Car.cs
@@ -2,170 +2,208 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Car : MonoBehaviour {
-	public delegate void CarEventHandler(Car car);
+public class Car : MonoBehaviour
+{
+    public delegate void CarEventHandler(Car car);
 
-	public event CarEventHandler Spawned, ReachedDestination;
+    public event CarEventHandler Spawned, ReachedDestination;
 
-	public Lane currentLane;
-	public SourceSink source, destination;
-	public float distanceOnLane;
-	public Car nextCar;
+    public Transform collision;
 
-	public CarState state;
-	public float speed;
+    public Lane currentLane;
+    public SourceSink source, destination;
+    public float distanceOnLane;
+    public Car nextCar;
 
-	Route route;
-	int route_index;
-	public ExplicitTurn NextTurn {
-		get { return route[route_index]; }
-	}
+    public CarState state;
+    public float speed;
 
-	CarAI ai;
+    Route route;
+    int route_index;
+    public ExplicitTurn NextTurn
+    {
+        get { return route[route_index]; }
+    }
 
-	// Use this for initialization
-	void Start () {
-		ai = new CarAI(this);
-		route_index = -1;
+    CarAI ai;
 
-		currentLane.Subscribe(this);
+    // Use this for initialization
+    void Start()
+    {
+        ai = new CarAI(this);
+        route_index = -1;
 
-		if(Spawned != null) {
-			Spawned(this);
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Some sort of pathfinding
-		if(route_index == -1) {
-			RecomputeRoute ();
-			Debug.Log ("Final Route: " + route);
-		}
+        currentLane.Subscribe(this);
 
-		if(IsAtDestination() && ReachedDestination != null) {
-			ReachedDestination(this);
-		}
+        if (Spawned != null)
+        {
+            Spawned(this);
+        }
+    }
 
-		ai.Update();
-		Move();
-		//	Collision prevention
-		//	Set speed
-		//	Move down lane
-		//	Turn at intersections
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        //Some sort of pathfinding
+        if (route_index == -1)
+        {
+            RecomputeRoute();
+            //Debug.Log ("Final Route: " + route);
+        }
 
-	bool IsAtDestination() {
-		return currentLane.to == destination && currentLane.length - distanceOnLane <= 0;
-	}
+        if (IsAtDestination() && ReachedDestination != null)
+        {
+            ReachedDestination(this);
+        }
 
-	void StartNewLane()
-	{
-	}
-	
-	void Move()
-	{
-		float trueSpeed = speed * Time.deltaTime;
-		Vector3 movement = currentLane.direction * trueSpeed;
+        ai.Update();
+        Move();
+        //	Collision prevention
+        //	Set speed
+        //	Move down lane
+        //	Turn at intersections
+    }
 
-		Intersection intersection = currentLane.to as Intersection;
+    bool IsAtDestination()
+    {
+        return currentLane.to == destination && currentLane.length - distanceOnLane <= 0;
+    }
 
-		if(intersection != null) {
-			float distanceToTurn = Vector3.Distance(transform.position, NextTurn.TurnPoint);
-			
-			//If a turn point will be reached special actions need to be taken
-			if(movement.magnitude > distanceToTurn) {
-				Lane newLane = NextTurn.LaneOut;	//Find new lane
-				float newDistanceOnLane = Vector3.Distance(NextTurn.TurnPoint, newLane.startPoint);
-				
-				//Move position and adjust car location internally
-				transform.position = NextTurn.TurnPoint;	//Drive to next turn
-				SwitchToLane(newLane);
-				distanceOnLane = -newDistanceOnLane;		//You start somewhat behind the start of the lane
+    void StartNewLane()
+    {
+    }
 
-				route_index++;
+    void Move()
+    {
+        float trueSpeed = speed * Time.deltaTime;
+        Vector3 movement = currentLane.direction * trueSpeed;
 
-				//Recalculate speed and movement
-				trueSpeed = movement.magnitude - distanceToTurn;	//Speed is now the rest after driving to the turn
-				movement = currentLane.direction * trueSpeed;	//Recalculate movement
-			}
-		}
+        Intersection intersection = currentLane.to as Intersection;
 
-		distanceOnLane += trueSpeed;
-		transform.Translate(movement);
-	}
-	void SwitchToLane(Lane lane) {
-		currentLane.Unsubcribe(this);
-		currentLane = lane;
-		currentLane.Subscribe(this);
-	}
+        if (intersection != null)
+        {
+            float distanceToTurn = Vector3.Distance(transform.position, NextTurn.TurnPoint);
 
-	//Recomputes route of the agent
-	//A queue of routes is kept. Then the first element is tested
-	//If this route ends in the destination this is the new route
-	//Otherwise all possible next turns are generated and the best lane chosen for new routes.
-	//These new routes are pushed at the end of the queue.
-	public void RecomputeRoute() {
-		Connection nextNode = currentLane.to;
-		Queue<Route> routes = new Queue<Route>(GenerateRoutesFromNextNode(nextNode));
+            //If a turn point will be reached special actions need to be taken
+            if (movement.magnitude > distanceToTurn)
+            {
+                Lane newLane = NextTurn.LaneOut;	//Find new lane
+                float newDistanceOnLane = Vector3.Distance(NextTurn.TurnPoint, newLane.startPoint);
 
-		while(routes.Count > 0) {
-			Route route = routes.Dequeue ();
-			if (route.EndPoint == destination) {
-				this.route = route;
-				route_index = 0;
-				return;
-			}
-			else {
-				ICollection<PossibleTurn> possibleNextTurns = route.PossibleNextTurns;
-				if(possibleNextTurns == null) {
-					continue;
-				}
-				foreach(PossibleTurn turn in possibleNextTurns) {
-					ExplicitTurn bestTurn = SelectBestExplicitTurn(turn);
-					Route new_route = new Route(route,bestTurn,ai);
-					routes.Enqueue(new_route);
-				}
-			}
-		}
-		route_index = 0;
-	}
+                //Move position and adjust car location internally
+                transform.position = NextTurn.TurnPoint;	//Drive to next turn
+                SwitchToLane(newLane);
+                distanceOnLane = -newDistanceOnLane;		//You start somewhat behind the start of the lane
 
-	ICollection<Route> GenerateRoutesFromNextNode(Connection nextNode) {
-		if(nextNode.GetType().Equals (typeof(SourceSink))) {
-			return null;
-		}
-		else {
-			List<Route> routes = new List<Route>();
-			Intersection intersection = (Intersection)nextNode;
-			foreach(PossibleTurn turn in intersection.PossibleTurns) {
-				if(turn.LaneIn == currentLane) {
-					ExplicitTurn bestTurn = SelectBestExplicitTurn(turn);
-					Route new_route = new Route(bestTurn,ai);
-					routes.Add (new_route);
-				}
-			}
-			return routes;
-		}
-	}
+                route_index++;
 
-	ExplicitTurn SelectBestExplicitTurn(PossibleTurn turn) {
-		ExplicitTurn argmax = default(ExplicitTurn);
-		float value_max = float.MinValue;
+                //Recalculate speed and movement
+                trueSpeed = movement.magnitude - distanceToTurn;	//Speed is now the rest after driving to the turn
+                movement = currentLane.direction * trueSpeed;	//Recalculate movement
+            }
+        }
 
-		for(int i = 0 ; i < turn.LanesOut.Length ; i++) {
-			float value = ai.EvaluateLane(turn.LanesOut[i]);
-			if(value > value_max) {
-				argmax = new ExplicitTurn(turn,i);
-				value_max = value;
-			}
-		}
-		return argmax;
-	}
+        distanceOnLane += trueSpeed;
+        transform.Translate(movement);
+    }
+    void SwitchToLane(Lane lane)
+    {
+        currentLane.Unsubcribe(this);
+        currentLane = lane;
+        currentLane.Subscribe(this);
+    }
+
+    //Recomputes route of the agent
+    //A queue of routes is kept. Then the first element is tested
+    //If this route ends in the destination this is the new route
+    //Otherwise all possible next turns are generated and the best lane chosen for new routes.
+    //These new routes are pushed at the end of the queue.
+    public void RecomputeRoute()
+    {
+        Connection nextNode = currentLane.to;
+        Queue<Route> routes = new Queue<Route>(GenerateRoutesFromNextNode(nextNode));
+
+        while (routes.Count > 0)
+        {
+            Route route = routes.Dequeue();
+            if (route.EndPoint == destination)
+            {
+                this.route = route;
+                route_index = 0;
+                return;
+            }
+            else
+            {
+                ICollection<PossibleTurn> possibleNextTurns = route.PossibleNextTurns;
+                if (possibleNextTurns == null)
+                {
+                    continue;
+                }
+                foreach (PossibleTurn turn in possibleNextTurns)
+                {
+                    ExplicitTurn bestTurn = SelectBestExplicitTurn(turn);
+                    Route new_route = new Route(route, bestTurn, ai);
+                    routes.Enqueue(new_route);
+                }
+            }
+        }
+        route_index = 0;
+    }
+
+    ICollection<Route> GenerateRoutesFromNextNode(Connection nextNode)
+    {
+        if (nextNode.GetType().Equals(typeof(SourceSink)))
+        {
+            return null;
+        }
+        else
+        {
+            List<Route> routes = new List<Route>();
+            Intersection intersection = (Intersection)nextNode;
+            foreach (PossibleTurn turn in intersection.PossibleTurns)
+            {
+                if (turn.LaneIn == currentLane)
+                {
+                    ExplicitTurn bestTurn = SelectBestExplicitTurn(turn);
+                    Route new_route = new Route(bestTurn, ai);
+                    routes.Add(new_route);
+                }
+            }
+            return routes;
+        }
+    }
+
+    ExplicitTurn SelectBestExplicitTurn(PossibleTurn turn)
+    {
+        ExplicitTurn argmax = default(ExplicitTurn);
+        float value_max = float.MinValue;
+
+        for (int i = 0; i < turn.LanesOut.Length; i++)
+        {
+            float value = ai.EvaluateLane(turn.LanesOut[i]);
+            if (value > value_max)
+            {
+                argmax = new ExplicitTurn(turn, i);
+                value_max = value;
+            }
+        }
+        return argmax;
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        //GameObject car = col.gameObject;
+        //Car carScript = col.GetComponent<Car>();
+        //if (carScript != null)
+        //    if (carScript.distanceOnLane > distanceOnLane)
+        //        Instantiate(collision, transform.position, Quaternion.identity);
+        //Destroy(gameObject);
+    }
 }
 
-public enum CarState {
-	DRIVING,
-	QUEUED,
-	ON_INTERSECTION
+public enum CarState
+{
+    DRIVING,
+    QUEUED,
+    ON_INTERSECTION
 }
---
+++ Traffic Flow Simulator/Assets/Collision.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour 
+{
+    public float towAwayTime = 5;
+    public GameObject tower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        Invoke("TowAway", towAwayTime);
+        GameObject tower = GameObject.Find("TowAwayText");
+        GUIText text = tower.GetComponent<GUIText>();
+        text.text = "Car currently being towed: yes";
+    }
+
+    void TowAway()
+    {
+        Destroy(gameObject);
+    }
+}
