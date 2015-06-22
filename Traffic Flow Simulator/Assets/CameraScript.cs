fe0fa364d2860a49996a4f90bf2f56a46ca73a9f
Timo Overbeek <timotheusoverbeek@gmail.com>
2015-06-22 19:25:02 +0200

    You can now move the camera by moving your mouse to the edge of the screen

--- Traffic Flow Simulator/Assets/CameraScript.cs
+++ Traffic Flow Simulator/Assets/CameraScript.cs
@@ -5,7 +5,10 @@ public class CameraScript : MonoBehaviour
 {
     public float min = -10;
     public float max = 10;
-    public float speed = 10f;
+    public float scrollSpeed = 10f;
+
+    public float moveDistance = 10;
+    public float moveSpeed = 10f;
 
 	// Use this for initialization
 	void Start () 
@@ -17,10 +20,19 @@ public class CameraScript : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
     {
-        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * speed;
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
         if (Camera.main.orthographicSize < min)
             Camera.main.orthographicSize = min;
         if (Camera.main.orthographicSize > max)
             Camera.main.orthographicSize = max;
+
+        if (Input.mousePosition.x < moveDistance)
+            Camera.main.transform.Translate(-moveSpeed, 0, 0);
+        if (Input.mousePosition.x > Screen.width - moveDistance)
+            Camera.main.transform.Translate(moveSpeed, 0, 0);
+        if (Input.mousePosition.y < moveDistance)
+            Camera.main.transform.Translate(0, -moveSpeed, 0);
+        if (Input.mousePosition.y > Screen.height - moveDistance)
+            Camera.main.transform.Translate(0, moveSpeed, 0);
 	}
 }
