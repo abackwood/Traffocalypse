eeb313535abf6c28d6c139de1feddb5425174ee9
Timo Overbeek <timotheusoverbeek@gmail.com>
2015-06-22 18:29:36 +0200

    Started with implementing camera control, by using scrolling to zoom in and out

---
+++ Traffic Flow Simulator/Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour 
+{
+    public float min = -10;
+    public float max = 10;
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        min = Camera.main.orthographicSize + min;
+        max = Camera.main.orthographicSize + max;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * speed;
+        if (Camera.main.orthographicSize < min)
+            Camera.main.orthographicSize = min;
+        if (Camera.main.orthographicSize > max)
+            Camera.main.orthographicSize = max;
+	}
+}
